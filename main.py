9e672c6cf6e864e90de8a7b9ec6cf577b39ae2dd
Nikita Podpruzhnikov <ferunelli222@gmail.com>
2020-03-07 01:20:50 +0300

    Eight picture flying over top border of the screen

--- main.py
+++ main.py
@@ -16,15 +16,21 @@ colors = {'White': [255, 255, 255],
 FPS = 60
 rotation = 0
 rotation_speed = 2
-
-eight_orig = pygame.image.load('eight.png')
+WIDTH_CENTER = SCREEN_WIDTH // 2
+HEIGHT_CENTER = SCREEN_HEIGHT // 2
+eight_orig = pygame.image.load('eight.png').convert_alpha()
 eight_orig.set_colorkey(colors['CWhite'])
 eight = eight_orig.copy()
 eight.set_colorkey(colors['CWhite'])
 rect = eight.get_rect()
 rect.center = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
 
+
 state = True
+eight_pressed = False
+rotation_flag = True
+eight_visibility = True
+
 while state:
     clock.tick(FPS)
     screen.fill(colors['CWhite'])
@@ -34,15 +40,30 @@ while state:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             pos = pygame.mouse.get_pos()
             if rect.collidepoint(pos):
-                rect.center = rect.center[0] + 50, rect.center[1]
-                pygame.draw.rect(screen, colors['CWhite'], rect, 1)
+                eight_pressed = True
+    if eight_pressed:
+        if rotation_speed <= 60 and rotation_flag:
+            rotation_speed += 0.3
+        else:
+            rotation_flag = False
 
-    old_center = rect.center
-    rotation = (rotation - rotation_speed) % 360
-    new_eight = pygame.transform.rotate(eight_orig, rotation)
-    rect = new_eight.get_rect()
-    rect.center = old_center
-    screen.blit(new_eight, rect)
+        old_center = rect.center
+        rotation = (rotation - rotation_speed) % 360
+        new_eight = pygame.transform.rotate(eight_orig, rotation)
+        rect = new_eight.get_rect()
+        rect.center = old_center
+        screen.blit(new_eight, rect)
+        if not rotation_flag:
+            if rect.bottom > 0:
+                HEIGHT_CENTER -= 5
+                rect.center = WIDTH_CENTER, HEIGHT_CENTER
+            else:
+                eight_pressed = False
+                eight_visibility = False
+    elif eight_visibility:
+        rect = eight_orig.get_rect()
+        rect.center = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
+        screen.blit(eight_orig, rect)
     pygame.display.flip()
 
 pygame.quit()
